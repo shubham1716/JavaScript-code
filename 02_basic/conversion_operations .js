fe41c0d788a84a586e53e5d4525998b9a9b0f6e6
shubham1716 <shubhamk7640@gmail.com>
2025-11-14 05:45:31 +0000

    Comment out initial variable examples in conversion_operations.js

--- 02_basic/conversion_operations .js
+++ 02_basic/conversion_operations .js
@@ -1,7 +1,37 @@
-let score ="33abc"
-console.log(typeof score);
+// let score ="33abc"
+// console.log(typeof score);
 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+// operations 
+let valve =10
+let negvalue = -valve
+// console.log(negvalue);
+
+
+// console.log(2+3);
+
+// console.log(2-3);
+
+// console.log(2*3);
+
+// console.log(2/3);
+
+// console.log(2%3);
+
+
+// console.log(2**3);
+
+
+console.log("2"+3);
+
+console.log(2+"3");
+
+console.log("2"+"3");
+
+console.log("2"+3+3);
+console.log(2+3+"3");
