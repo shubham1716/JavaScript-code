36e17c7ab653b77f6ae45c0738d6a23f48bb08ff
shubham1716 <shubhamk7640@gmail.com>
2025-11-15 04:04:00 +0000

    Add comparison operations examples in comparison.js

---
+++ 02_basic/comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("shubham");
+
+// console.log("2">1);
+// console.log("01">1);
+
+// avoid always this type conversion
+
+// console.log(null >1);
+
+// console.log(null==1);
+
+// console.log(null >=1);
+
+// console.log(null<1);
+
+// end
+
+///  ===
+console.log("2"===2);  // false
+
+console.log("2"==2); // true
