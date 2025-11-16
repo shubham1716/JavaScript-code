3e632d77e2a4a6e8a8b5d549a208fb40189323d9
shubham1716 <shubhamk7640@gmail.com>
2025-11-16 06:10:52 +0000

    Add nums-and-maths.js with basic math operations and examples

---
+++ 02_basic/nums-and-maths.js
@@ -0,0 +1,23 @@
+const score = 10;
+// console.log(score);
+
+
+const num = new Number(100);
+// console.log(num);
+
+// console.log(num.toString().length);
+// 
+
+const no = 1234.2323;
+console.log(no.toPrecision(4));
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    +++++++++++++++++++++++++++++++++
+
+//maths
+console.log(Math);
+
+console.log(Math.PI);
+console.log(Math.abs(-2));
+console.log(Math.round(2.32));
+
