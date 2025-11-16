dcffb362287d48021861f4f9a59cc9d2269be502
shubham1716 <shubhamk7640@gmail.com>
2025-11-16 05:38:32 +0000

    Add string manipulation examples in string.js

---
+++ 02_basic/string.js
@@ -0,0 +1,28 @@
+const name ="shubham"
+let repocount = 13;
+
+// console.log(name + repocount);
+// console.log(`my name is ${name}`);
+
+const newgame = new  String('shubham kumar')
+// console.log(newgame.__proto__);
+
+// console.log(newgame.length);
+// console.log(newgame.charAt(2));
+// console.log(newgame.indexOf('h'));
+
+
+const newstring = newgame.substring(1,5)
+// console.log(newstring);
+
+
+const newstring1 ="    shubham   "
+// console.log(newstring1)
+// console.log(newstring1.trim());
+
+const url ="https://-shu-bh-am_kum-ar"
+// console.log(url.replace('_','@'))
+// console.log(url.includes('shubham'))
+
+console.log(url.split('-'));
+
