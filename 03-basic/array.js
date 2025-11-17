19a41181302c0c86d197c22928b93b5294322958
shubham1716 <shubhamk7640@gmail.com>
2025-11-17 06:13:30 +0000

    Add array.js with examples of array methods and operations

---
+++ 03-basic/array.js
@@ -0,0 +1,24 @@
+//array
+
+let array =[1,2,3,4,5,"shubham"]
+// console.log(array);
+
+// //array method
+
+// array.push("kumar")
+// console.log(array)
+
+// array.pop()
+// console.log(array);
+
+// array.unshift(9)
+// array.shift()
+// console.log(array);
+
+let n1=array.slice(1,3)
+console.log(n1)
+
+let n2=array.splice(1,3)
+console.log(array);
+console.log(n2);
+
