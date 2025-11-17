f4f0a7ba2f6cf79979326da8361e5d17331cbc8d
shubham1716 <shubhamk7640@gmail.com>
2025-11-17 06:29:03 +0000

    Comment out array method examples in array.js for clarity

--- 03-basic/array.js
+++ 03-basic/array.js
@@ -15,10 +15,22 @@ let array =[1,2,3,4,5,"shubham"]
 // array.shift()
 // console.log(array);
 
-let n1=array.slice(1,3)
-console.log(n1)
+// let n1=array.slice(1,3)
+// console.log(n1)
 
-let n2=array.splice(1,3)
-console.log(array);
-console.log(n2);
+// let n2=array.splice(1,3)
+// console.log(array);
+// console.log(n2);
+
+// //concat
+// let arr1=[1,2,3]
+// let arr2=[4,5,6]
+// let arr3=arr1.concat(arr2)
+// console.log(arr3);
+
+
+// //spread operator
+// let arr4=[...arr1,...arr2]
+// console.log(arr4);
+
 
